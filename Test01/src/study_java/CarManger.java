92a5c2313dc48dcc76bb9a75a879588c766c5ddd
DokHakMaster <solodokhak@gmail.com>
2022-01-04 20:40:23 +0900

    car test

---
+++ Test01/src/study_java/CarManger.java
@@ -0,0 +1,17 @@
+package study_java;
+
+public class CarManger {
+	
+	public static void main() {
+	
+		Car dogCar = new Car();
+		
+		dogCar.speedUp();
+		dogCar.speedUp();
+		System.out.println(dogCar.getCurrentSpeed());
+		
+		
+		
+	}
+
+}
